b61077310eb24efd5c7ec599e92495bab048b8d3
Wojciech <wenter2005pl@gmail.com>
2025-03-10 14:22:48 +0100

    Lab2

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        Point p2=new Point(7,8);
+        p2.x=3;
+        p2.y=4;
+        System.out.println(p2.toString());
+
+
+        }
+    }
